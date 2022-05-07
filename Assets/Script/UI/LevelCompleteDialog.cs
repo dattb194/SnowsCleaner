1724484d8692344c8a16d3fdf0658ed805711de5
zef le <91973815+zefle@users.noreply.github.com>
2022-05-07 11:36:18 +0700

    add level complete

---
+++ Assets/Script/UI/LevelCompleteDialog.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelCompleteDialog : MonoBehaviour
+{
+    public Transform [] star= new Transform[3];
+    public UnityEngine.UI.Text txtLv;
+    public void OnSetData(int _lv, int _rate)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            star[i].gameObject.SetActive(false);
+        }
+        for (int i = 0; i < _rate; i++)
+        {
+            star[i].gameObject.SetActive(true);
+        }
+        txtLv.text = "Level " + _lv;
+
+        gameObject.SetActive(true);
+    }
+}
